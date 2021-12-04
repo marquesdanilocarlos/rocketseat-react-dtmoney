6a376fa96fd401154a8920666a9e4f7ecc4c73a6
Danilo Carlos Marques <danilo.marques@presidencia.gov.br>
2021-12-03 21:35:09 -0300

    Criacao de estilos globais com styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import React from "react";
-
+import { GlobalStyle } from "./styles/global";
 export function App() {
     return (
         <div className="App">
+            <GlobalStyle />
             <h1>Hello World!</h1>
         </div>
     );
