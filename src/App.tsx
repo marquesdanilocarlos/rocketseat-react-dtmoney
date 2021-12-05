a5501e522d33ea25260eeb494de5a4893268529d
Danilo Carlos Marques <danilo.marques@presidencia.gov.br>
2021-12-04 23:56:18 -0300

    Adicao inicial do modal de insercao de transacoes

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,36 @@ import { Dashboard } from "./components/Dashboard";
 import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
+import Modal from "react-modal";
+import { useState } from "react";
+
 export function App() {
+    Modal.setAppElement("#root");
+
+    const [newTransactionModalIsOpen, setNewTransactionModalIsOpen] =
+        useState(false);
+
+    function handleOpenNewTransactionModal() {
+        setNewTransactionModalIsOpen(true);
+    }
+
+    function handleCloseTransactionModal() {
+        setNewTransactionModalIsOpen(false);
+    }
+
     return (
         <>
             <GlobalStyle />
-            <Header />
+            <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
             <Dashboard />
             <Footer />
+
+            <Modal
+                isOpen={newTransactionModalIsOpen}
+                onRequestClose={handleCloseTransactionModal}
+            >
+                <h2>Cadastrar transação</h2>
+            </Modal>
         </>
     );
 }
