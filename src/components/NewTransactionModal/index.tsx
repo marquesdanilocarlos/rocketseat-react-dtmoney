e3331767550702c4294afb874fa56377842060b4
Danilo Carlos Marques <danilo.marques@presidencia.gov.br>
2021-12-05 09:54:38 -0300

    Criacao e estilizacao do formulario do modal

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,4 +1,8 @@
 import Modal from "react-modal";
+import { Container, TransactionTypeContainer } from "./styles";
+import closeImg from "../../assets/close.svg";
+import incomeImg from "../../assets/income.svg";
+import outcomeImg from "../../assets/outcome.svg";
 
 interface NewTransactionModalProps {
     isOpen: boolean;
@@ -12,8 +16,52 @@ export function NewTransactionModal({
     Modal.setAppElement("#root");
 
     return (
-        <Modal isOpen={isOpen} onRequestClose={onRequestClose}>
-            <h2>Cadastrar transação</h2>
+        <Modal
+            isOpen={isOpen}
+            onRequestClose={onRequestClose}
+            overlayClassName="react-modal-overlay"
+            className="react-modal-content"
+        >
+            <Container>
+                <button
+                    type="button"
+                    onClick={onRequestClose}
+                    className="react-modal-close"
+                >
+                    <img src={closeImg} alt="Fechar" />
+                </button>
+                <h2>Cadastrar transação</h2>
+                <input
+                    className="form-control form-control-lg mb-3"
+                    type="text"
+                    placeholder="Título"
+                />
+                <input
+                    className="form-control form-control-lg mb-3"
+                    type="number"
+                    placeholder="Valor"
+                />
+                <TransactionTypeContainer
+                    className="btn-group mb-3"
+                    role="group"
+                    aria-label="Tipo de transação"
+                >
+                    <button type="button" className="btn btn-lg btn-light">
+                        <img src={incomeImg} alt="Entrada" />
+                        <span className="ms-1">Entrada</span>
+                    </button>
+                    <button type="button" className="btn btn-lg btn-light">
+                        <img src={outcomeImg} alt="Saída" className="mr-1" />
+                        <span className="ms-1">Saída</span>
+                    </button>
+                </TransactionTypeContainer>
+                <input
+                    className="form-control form-control-lg mb-3"
+                    type="text"
+                    placeholder="Categoria"
+                />
+                <button className="btn btn-success btn-lg ">Cadastrar</button>
+            </Container>
         </Modal>
     );
 }
--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -1,3 +1,30 @@
 import styled from "styled-components";
 
-export const Modal = styled.div``;
+export const Container = styled.form`
+    h2 {
+        color: var(--text-title);
+        font-size: 1.5rem;
+        margin-bottom: 2rem;
+    }
+
+    input::placeholder {
+        color: var(--text-body);
+    }
+
+    .btn-success {
+        width: 100%;
+        background: var(--bs-success);
+        border-color: var(--bs-success);
+    }
+`;
+
+export const TransactionTypeContainer = styled.div`
+    width: 100%;
+    .btn {
+        border: 1px solid var(--text-body);
+        border-radius: 0.25rem;
+        &:hover {
+            filter: brightness(90%);
+        }
+    }
+`;
