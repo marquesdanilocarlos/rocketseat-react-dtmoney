0c6163f11919d5b869cad2111699e6fa26bd9353
Danilo Carlos Marques <danilo.marques@presidencia.gov.br>
2021-12-05 12:40:23 -0300

    Criacao de cadastro de transaction na fake api

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -8,6 +8,7 @@ import closeImg from "../../assets/close.svg";
 import incomeImg from "../../assets/income.svg";
 import outcomeImg from "../../assets/outcome.svg";
 import { FormEvent, useState } from "react";
+import { api } from "../../services/api";
 
 interface NewTransactionModalProps {
     isOpen: boolean;
@@ -38,7 +39,7 @@ export function NewTransactionModal({
 
     function handleCreateNewTransaction(event: FormEvent) {
         event.preventDefault();
-        console.log(transaction);
+        api.post("/transactions", transaction);
     }
 
     return (
